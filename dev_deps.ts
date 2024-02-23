ee89071290c04b413f2fc3eb28a59e10d25df252
Joel Walker <joelwalker1995@gmail.com>
2024-02-23 15:49:09 -0800

    Updated deps

--- dev_deps.ts
+++ dev_deps.ts
@@ -1,4 +1,4 @@
-export * as fs from 'https://deno.land/std@0.216.0/fs/mod.ts';
-export * as path from 'https://deno.land/std@0.216.0/path/mod.ts';
-export * as http from 'https://deno.land/std@0.216.0/http/mod.ts';
+export * as fs from 'https://deno.land/std@0.217.0/fs/mod.ts';
+export * as path from 'https://deno.land/std@0.217.0/path/mod.ts';
+export * as http from 'https://deno.land/std@0.217.0/http/mod.ts';
 export * as dnt from 'https://deno.land/x/dnt@0.39.0/mod.ts';
--- test_deps.ts
+++ test_deps.ts
@@ -1,3 +1,3 @@
-export * as async from 'https://deno.land/std@0.216.0/async/mod.ts';
-export * as assert from 'https://deno.land/std@0.216.0/assert/mod.ts';
-export * as data_structures from 'https://deno.land/std@0.216.0/data_structures/mod.ts';
+export * as async from 'https://deno.land/std@0.217.0/async/mod.ts';
+export * as assert from 'https://deno.land/std@0.217.0/assert/mod.ts';
+export * as data_structures from 'https://deno.land/std@0.217.0/data_structures/mod.ts';
