0c4faf073f3803db0578bba29a24a08dd672a2ca
Joel Walker <joelwalker1995@gmail.com>
2026-03-16 20:47:58 -0700

    More twiddling

--- src/core/SparseMask.ts
+++ src/core/SparseMask.ts
@@ -1,6 +1,6 @@
 import { searchSorted } from '../util/sorted.ts';
 
-interface Chunk {
+interface ChunkBase {
   offset: number;
   size: number;
   population: number;
@@ -8,57 +8,51 @@ interface Chunk {
   indexDeltas?: number[];
 }
 
-export class SparseMask {
-  private chunks: Chunk[];
+export class SparseMask<ChunkType extends ChunkBase> {
+  private chunks: ChunkType[];
 
-  constructor(size: number, population: number) {
-    this.chunks = [{ offset: 0, size, population }];
+  constructor(initialChunk: ChunkType) {
+    this.chunks = [initialChunk];
   }
 
-  set(offset: number, size: number, indexDeltas: number[]): void {
-    const insertAt = this.locateChunk(offset);
+  set(chunk: ChunkType): void {
+    const insertAt = this.locateChunk(chunk.offset);
     if (insertAt >= this.chunks.length) {
-      throw new Error(`Offset ${offset} is out of range`);
+      throw new Error(`Offset ${chunk.offset} is out of range`);
     }
-    const chunk = this.chunks[insertAt];
-    if (chunk.indexDeltas !== undefined || size > chunk.size) {
-      throw new Error(`Chunk at offset ${offset} is already set`);
+    const oldChunk = this.chunks[insertAt];
+    if (oldChunk.indexDeltas !== undefined || chunk.size > oldChunk.size) {
+      throw new Error(`Chunk at offset ${chunk.offset} is already set`);
     }
-    if (chunk.offset === offset && chunk.size === size) {
-      if (chunk.population !== indexDeltas.length) {
-        throw new Error(`Chunk at offset ${offset} has the wrong population`);
+    if (oldChunk.offset === chunk.offset && oldChunk.size === chunk.size) {
+      if (oldChunk.population !== chunk.population) {
+        throw new Error(`Chunk at offset ${chunk.offset} has the wrong population`);
       }
-      chunk.indexDeltas = indexDeltas;
-    } else if (chunk.offset === offset) {
-      chunk.offset += size;
-      chunk.size -= size;
-      chunk.population -= indexDeltas.length;
-      this.chunks.splice(insertAt, 0, {
-        offset,
-        size,
-        population: indexDeltas.length,
-        indexDeltas,
-      });
-    } else if (offset + size === chunk.offset + chunk.size) {
-      chunk.size -= size;
-      chunk.population -= indexDeltas.length;
-      this.chunks.splice(insertAt + 1, 0, {
-        offset,
-        size,
-        population: indexDeltas.length,
-        indexDeltas,
-      });
-    }else{
+      oldChunk.indexDeltas = chunk.indexDeltas;
+    } else if (oldChunk.offset === chunk.offset) {
+      oldChunk.offset += chunk.size;
+      oldChunk.size -= chunk.size;
+      oldChunk.population -= chunk.population;
+      this.chunks.splice(insertAt, 0, chunk);
+    } else if (chunk.offset + chunk.size === oldChunk.offset + oldChunk.size) {
+      oldChunk.size -= chunk.size;
+      oldChunk.population -= chunk.population;
+      this.chunks.splice(insertAt + 1, 0, chunk);
+    } else {
       throw new Error(`Internal error`); // Should never happen
     }
-    if (chunk.population < 0) {
+    if (oldChunk.population < 0) {
       throw new Error(`Children have higher population than the parent`);
     }
   }
 
-  countZerosLt(indices: number[]){}
+  countZerosLt(indices: number[]): (number | ChunkType)[] {
+    return [];
+  }
 
-  indexNthZero(n: number[]){}
+  indexNthZero(n: number[]): (number | ChunkType)[] {
+    return [];
+  }
 
   private locateChunk(offset: number): number {
     return searchSorted(this.chunks, (c) => c.offset >= offset);
